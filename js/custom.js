af93b98acbaf324fe97fd05b3f75bc55dfa31212
Jasper Smit <0911798@hr.nl>
2016-05-29 17:02:21 +0200

    Padding && Form fix

--- js/custom.js
+++ js/custom.js
@@ -5,17 +5,25 @@ $(document).ready(function(){
 		var sUsername = $("#username").val();
 		var sPassword = $("#password").val();
 
-		var data = { username: sUsername, password: sPassword };
 
-		$.post("index.php", data, 
-			function(returnData){
-				console.log(returnData['password_matches']);
-				if(returnData['password_matches']){
-					$(".msg").css('color', 'red');
-				}else{
-					$(".msg").css('color', 'pink');
-				}
-		}, "json" );
+        if(sUsername != '' && sPassword != ''){
+            console.log(sUsername + sPassword);
+            var data = { username: sUsername, password: sPassword };
+
+            $.post("index.php", data,
+                function(returnData){
+                    console.log(returnData['password_matches']);
+                    if(returnData['password_matches']){
+                        $(".msg").css('color', 'red');
+                    }else{
+                        $(".msg").css('color', 'pink');
+                    }
+                }, "json" );
+        }
+
+
 	})
+
+
 		
 });
