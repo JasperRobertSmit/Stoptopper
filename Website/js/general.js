f32d151a5242d79350b20cbae7c42896cc62083e
JasperRobertSmit <0911798@hr.nl>
2016-06-07 16:05:26 +0200

    ErrorMessage Improvements

--- Website/js/general.js
+++ Website/js/general.js
@@ -16,16 +16,18 @@ function updateFields(oResult, redirectUrl, redirectMessage){
 
     if(!oResult['Success']){
         //Er is een fout
-        if(oResult['Message'] == "Username already exists!"){
+        if(oResult['field'] == "Username already exists!"){
             var username = $("#username");
             username.addClass('validation-failed');
             username.parent().find('.validation-message').text(oResult['Message']);
         }
 
-        if(oResult['Message'] == "Invalid credentials!"){
-            var usernameAndPassword = $("#username, #password");
-            usernameAndPassword.addClass('validation-failed');
-            usernameAndPassword.parent().find('.validation-message').text(oResult['Message']);
+        if(oResult['ERRORFIELD'] != null){
+            var errorfield = '#' + oResult['ERRORFIELD'];
+            console.log(errorfield);
+            
+            errorfield.addClass('validation-failed');
+            errorfield.parent().find('.validation-message').text(oResult['Message']);
         }
     }else{
         if(redirectUrl != null){
